248e601fb12e2e9b26d0a067da5c26e4384abd32
abhishek2001panwar <abhishek.panwar.cseiot.2022@miet.ac.in>
2024-12-15 10:59:59 +0530

    Configure ESLint settings and remove unused BackgroundBeamsDemo component

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
+  eslint: {
+    ignoreDuringBuilds: true, // Disable ESLint warnings during production builds
+    '@typescript-eslint/no-explicit-any': 'off', // Example rule to disable
+  },
   images: {
     domains: ['images.unsplash.com' ,'assets.aceternity.com' ,'plus.unsplash.com'], // Add allowed image domains here
   },
