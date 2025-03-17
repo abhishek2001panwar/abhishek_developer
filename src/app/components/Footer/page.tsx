1d144affb4bcb16a8c50686da8473035d57bf661
abhishek2001panwar <abhishek.panwar.cseiot.2022@miet.ac.in>
2025-03-17 20:20:40 +0530

    Add welcome message to Footer component and update Next.js version in package.json

--- src/app/components/Footer/page.tsx
+++ src/app/components/Footer/page.tsx
@@ -40,6 +40,12 @@ export default function Footer() {
         </Link>
       </div>
       <Social />
+      
+
+      <div>
+      <h1>Welcome to My Next.js App</h1>
+    </div>
+
     </div>
   );
 }
